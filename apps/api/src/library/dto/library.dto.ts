fe4753037116ea0afaf2c13135e2a27414c8cebc
Yann <yann.gellner@gmail.Com>
2026-05-18 11:47:15 +0200

    feat(api): manage library statuses

---
+++ apps/api/src/library/dto/library.dto.ts
@@ -0,0 +1,48 @@
+import { MediaType, WatchStatus } from '@prisma/client';
+import {
+  IsBoolean,
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
+
+export class SetStatusDto {
+  @IsInt()
+  tmdbId!: number;
+
+  @IsEnum(MediaType)
+  mediaType!: MediaType;
+
+  @IsEnum(WatchStatus)
+  status!: WatchStatus;
+}
+
+export class CreateListDto {
+  @IsString()
+  @Length(2, 80)
+  name!: string;
+
+  @IsBoolean()
+  isPublic!: boolean;
+}
+
+export class UpdateListDto {
+  @IsOptional()
+  @IsString()
+  @Length(2, 80)
+  name?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isPublic?: boolean;
+}
+
+export class ListItemDto {
+  @IsInt()
+  tmdbId!: number;
+
+  @IsEnum(MediaType)
+  mediaType!: MediaType;
+}
